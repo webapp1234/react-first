914ff8d85f7f3f12310dcb378b4ee98504f84f1d
webapp <webappstd@gmail.com>
2024-05-09 18:31:55 +0530

    usestate

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const About = () => {
-  return <div>About</div>;
+  let [count, setcount] = useState(0);
+
+  function increase() {
+    setcount(count + 1);
+  }
+
+  function descrease() {
+    setcount(count - 1);
+  }
+
+  return (
+    <>
+      <div>{count}</div>
+      <button onClick={increase}>increase</button>
+      <button onClick={descrease}>descrease</button>
+    </>
+  );
 };
 
 export default About;
--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  // const [name, setname] = useState("");
+  // const [password, setpassword] = useState("");
+
+  const [input, setinput] = useState({});
+
+  const [data, setdata] = useState([
+    {
+      name: "garvit",
+      password: "123",
+    },
+    {
+      name: "ram",
+      password: "123",
+    },
+  ]);
+
+  function handle(e) {
+    setinput({ ...input, [e.target.name]: e.target.value });
+  }
+
+  function add() {
+    setdata([...data, input]);
+  }
+
+  return (
+    <div>
+      {/* <input type="text" onChange={(e) => setname(e.target.value)} />
+      <input type="text" onChange={(e) => setpassword(e.target.value)} />
+      <button onClick={add}>add</button> */}
+
+      <input type="text" name="name" onChange={handle} />
+      <input type="text" name="password" onChange={handle} />
+      <button onClick={add}>add</button>
+
+      {data.map((val, index) => {
+        return (
+          <>
+            <h1>{val.name}</h1>
+            <h1>{val.password}</h1>
+          </>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Contact;
